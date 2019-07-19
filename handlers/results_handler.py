f3eb6c98385dde4f68bddd652f09884aa9826a06
Craig Quiter <cquiter@gmail.com>
2019-07-18 17:42:33 -0700

    Post gist and mark eval complete at end of results handling

--- handlers/results_handler.py
+++ handlers/results_handler.py
@@ -1,12 +1,20 @@
 import time
+from typing import Tuple, Optional
 
 from botleague_helpers.key_value_store import get_key_value_store, \
     SimpleKeyValueStore
 from box import Box
+from github import Github
+import github.Gist
 
 from bot_eval import get_eval_db_key
 import constants
-from models.eval_data import get_eval_data
+from models.eval_data import get_eval_data, save_eval_data
+import logging as log
+
+from utils import trigger_leaderboard_generation
+
+log.basicConfig(level=log.INFO)
 
 
 def handle_results_request(request):
@@ -15,8 +23,12 @@ def handle_results_request(request):
     """
     data = Box(request.json)
     kv = get_key_value_store()
-    error, results = process_results(data, kv)
+    error, results, eval_data = process_results(data, kv)
 
+    gist = post_results_to_gist(kv, results)
+    eval_data.status = constants.EVAL_STATUS_COMPLETE
+    save_eval_data(eval_data, kv)
+    trigger_leaderboard_generation()
     if error.msg:
         request.response.status = error.http_status_code
         results.error = error
@@ -25,15 +37,29 @@ def handle_results_request(request):
 
     return results
 
-    # TODO: Post results to gist
-    # TODO: Set status on eval_data to complete
+
+def post_results_to_gist(kv, results) -> Optional[github.Gist.Gist]:
+    if blconfig.is_test or get_test_name_from_callstack():
+        log.info('DETECTED TEST MODE: Not uploading results.')
+        ret = None
+    else:
+        github_client = Github(
+            kv.get(constants.BOTLEAGUE_RESULTS_GITHUB_TOKEN_NAME))
+        ret = github_client.get_user().create_gist(
+            public=True,
+            files={'results.json': github.InputFileContent(
+                results.to_json(indent=2))},
+            description='Automatically uploaded by botleague liaison')
+    return ret
 
 
-def process_results(result_payload: Box, kv: SimpleKeyValueStore):
+def process_results(result_payload: Box,
+                    kv: SimpleKeyValueStore) -> Tuple[Box, Box, Box]:
     eval_key = result_payload.get('eval_key', '')
     results = result_payload.get('results', Box())
     results.finished = time.time()
     error = Box(default_box=True)
+    eval_data = Box()
     if not eval_key:
         error.http_status_code = 400
         error.msg = 'eval_key must be in JSON data payload'
@@ -60,7 +86,7 @@ def process_results(result_payload: Box, kv: SimpleKeyValueStore):
             error.http_status_code = 400
             error.msg = 'Eval data status unknown %s' % eval_data.status
 
-    return error, results
+    return error, results, eval_data
 
 
 def add_eval_data_to_results(eval_data: Box, results: Box):
