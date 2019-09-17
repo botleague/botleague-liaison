935e3f04c638c680ac8da88eb788a19be28ae3fc
Craig Quiter <cquiter@gmail.com>
2019-09-17 13:12:56 -0700

    Problem CI working from deepdrive-sim

--- handlers/problem_ci_status_handler.py
+++ handlers/problem_ci_status_handler.py
@@ -4,19 +4,22 @@ from box import Box
 
 from problem_ci import get_problem_ci_db_id
 from responses.error import Error
-from utils import get_liaison_db_store
+from utils import get_liaison_db_store, dbox
 
 
 def handle_problem_ci_status_request(request) -> Tuple[Box, Error]:
-    data = Box(request.json)
-    commit = data.commit
-    pr_number = data.pr_number
+    if 'id' in request.params:
+        problem_ci_id = request.params['id']
+    else:
+        data = Box(request.json)
+        commit = data.commit
+        pr_number = data.pr_number
+        problem_ci_id = get_problem_ci_db_id(pr_number, commit)
     db = get_liaison_db_store()
     error = Error()
-    problem_ci_id = get_problem_ci_db_id(pr_number, commit)
-    problem_ci = db.get(problem_ci_id)
-
+    problem_ci = dbox(db.get(problem_ci_id))
     body = Box(status=problem_ci.status,
-               created_at=str(problem_ci.created_at))
+               created_at=str(problem_ci.created_at),
+               gists=problem_ci.gists or [])
     return body, error
 
