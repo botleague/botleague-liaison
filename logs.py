e0fe37ada85b56e3652a6ddf57f06c9a850c6ba4
Craig Quiter <cquiter@gmail.com>
2019-10-09 16:35:38 -0700

    Name slack messages by project

--- logs.py
+++ logs.py
@@ -1,4 +1,4 @@
 from loguru import logger as log
 from botleague_helpers.logs import add_slack_error_sink
 
-add_slack_error_sink(log, '#deepdrive-alerts')
+add_slack_error_sink(log, '#deepdrive-alerts', log_name='Botleague Liaison')
